f1d4ca6bbbc56aa7be402805785e79577eed7f2a
Peter Yetti <pyetti@users.noreply.github.com>
2017-06-08 00:53:23 -0400

    Added Cache for testing and simplified timer setup

--- src/main/java/com/khiya/cache/prune/CachePruner.java
+++ src/main/java/com/khiya/cache/prune/CachePruner.java
@@ -4,29 +4,48 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-import com.khiya.cache.Cache;
+import com.khiya.cache.CacheContainer;
 
 public class CachePruner extends TimerTask implements Pruner {
-	
-	private final Timer timer;
+
+	private final boolean isRunning;
 	private final CachePruningRunner cachePruningRunner;
 	private final ExecutorService executor;
-	
-	public CachePruner(Timer timer) {
-		this.timer = timer;
+	private final Timer timer = new Timer();
+
+	public CachePruner(long delay, long period) {
 		this.cachePruningRunner = new CachePruningRunner();
 		this.executor = Executors.newFixedThreadPool(1);
+		this.isRunning = startTask(delay, period);
+	}
+
+	public CachePruner(long delay, TimeUnit delayUnit, long period, TimeUnit periodUnit) {
+		this.cachePruningRunner = new CachePruningRunner();
+		this.executor = Executors.newFixedThreadPool(1);
+		this.isRunning = startTask(delay, delayUnit, period, periodUnit);
 	}
 
 	public void prune() {
-		Cache.prune();
+		CacheContainer.getInstance().prune();
 	}
 
-	@Override
-	public void startTask(long delay, long period) {
+	private boolean startTask(long delay, long period) {
+		if (period > 1000) {
+			timer.cancel();
+			this.timer.purge();
+			this.timer.schedule(this, delay, period);
+			return true;
+		}
+		return false;
+	}
+
+	private boolean startTask(long delay, TimeUnit delayUnit, long period, TimeUnit periodUnit) {
+		this.timer.cancel();
 		this.timer.purge();
-		this.timer.schedule(this, delay, period);
+		this.timer.schedule(this, delayUnit.toMillis(delay), periodUnit.toMillis(period));
+		return true;
 	}
 
 	@Override
@@ -34,12 +53,39 @@ public class CachePruner extends TimerTask implements Pruner {
 		executor.execute(cachePruningRunner);
 	}
 
+	public Timer getTimer() {
+		return timer;
+	}
+
+	public boolean isRunning() {
+		return isRunning;
+	}
+
+	@Override
+	public void finalize() {
+		executor.shutdown(); // Disable new tasks from being submitted
+		try {
+			// Wait a while for existing tasks to terminate
+			if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+				executor.shutdownNow(); // Cancel currently executing tasks
+				// Wait a while for tasks to respond to being cancelled
+				if (!executor.awaitTermination(60, TimeUnit.SECONDS))
+					System.err.println("executor did not terminate");
+			}
+		} catch (InterruptedException ie) {
+			// (Re-)Cancel if current thread also interrupted
+			executor.shutdownNow();
+			// Preserve interrupt status
+			Thread.currentThread().interrupt();
+		}
+	}
+
 	private class CachePruningRunner implements Runnable {
 
 		public void run() {
 			prune();
 		}
-		
+
 	}
 
 }
--- src/test/java/com/khiya/integration/CacheIT.java
+++ src/test/java/com/khiya/integration/CacheIT.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
-import com.khiya.cache.Cache;
+import com.khiya.cache.CacheContainer;
 
 public class CacheIT {
 
@@ -13,28 +13,28 @@ public class CacheIT {
 	public void testCache() {
 		RandoObject rando = new RandoObject("RandoThing", 1);
 
-		Cache.store("rando", rando);
-		RandoObject returnedToMe = Cache.get("rando", RandoObject.class);
+		CacheContainer.getInstance().store("rando", rando);
+		RandoObject returnedToMe = CacheContainer.getInstance().get("rando", RandoObject.class);
 		assertNotNull(returnedToMe);
 		assertEquals("RandoThing", returnedToMe.getThing());
 		assertEquals(1, returnedToMe.getOtherThing());
 
 		RandoObject rando2 = new RandoObject("RandoThing2", 2);
 
-		Cache.store("rando2", rando2);
-		RandoObject returnedToMe2 = Cache.get("rando2", RandoObject.class);
+		CacheContainer.getInstance().store("rando2", rando2);
+		RandoObject returnedToMe2 = CacheContainer.getInstance().get("rando2", RandoObject.class);
 		assertNotNull(returnedToMe2);
 		assertEquals("RandoThing2", returnedToMe2.getThing());
 		assertEquals(2, returnedToMe2.getOtherThing());
 
-		Cache.store("rando", rando);
-		returnedToMe = Cache.get("rando", RandoObject.class);
+		CacheContainer.getInstance().store("rando", rando);
+		returnedToMe = CacheContainer.getInstance().get("rando", RandoObject.class);
 		assertNotNull(returnedToMe);
 		assertEquals("RandoThing", returnedToMe.getThing());
 		assertEquals(1, returnedToMe.getOtherThing());
 
-		Cache.store("rando2", rando2);
-		returnedToMe2 = Cache.get("rando2", RandoObject.class);
+		CacheContainer.getInstance().store("rando2", rando2);
+		returnedToMe2 = CacheContainer.getInstance().get("rando2", RandoObject.class);
 		assertNotNull(returnedToMe2);
 		assertEquals("RandoThing2", returnedToMe2.getThing());
 		assertEquals(2, returnedToMe2.getOtherThing());
